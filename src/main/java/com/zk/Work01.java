0bcca6d692b3d1c3c58788a4bc9d9d8cec261877
fanlun007 <13058100289@163.com>
2018-09-13 21:42:31 +0800

    finish

--- src/main/java/com/zk/Work01.java
+++ src/main/java/com/zk/Work01.java
@@ -1,17 +1,19 @@
 package com.zk;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 @Slf4j
 public class Work01 {
 
-    public void input(){
+    public void input() {
 
         boolean flag = true;
-        while (flag){
+        while (flag) {
             int low = 0;
             int high = 0;
 
@@ -26,49 +28,76 @@ public class Work01 {
                     log.info("起始值>终止值: 重新输入");
                     continue;
                 }
-            } else{
+            } else {
                 log.info("非法字符,请输入数字");
                 continue;
             }
             log.info("起始数值：{}", low);
             log.info("终止数值：{}", high);
-            log.info("选择两个整数整除");
-            log.info("输入两个除数整数:");
             boolean invalidDiv = true;
-            while (invalidDiv){
+            String[] arr_first = null;
+            while (invalidDiv) {
+                log.info("输入两个除数整数:");
                 inScan = new Scanner(System.in);
-                String[] arr = inScan.nextLine().split("\\s+");
-                if (checkNum(arr)) {
-
+                String[] two_div = new Scanner(System.in).nextLine().split("\\s+");
+                if (checkNum(two_div)) {
+                    arr_first = two_div;
+                    invalidDiv = false;
+                } else {
+                    log.info("输入除数不为数字或包含0,重新输入");
+                }
+            }
+            if (!StringUtils.isEmpty(arr_first)) {
+                List<Integer> canList = new ArrayList<>();
+                for (int i = low; i <= high; i++) {
+                    if (canDivInArr(i, arr_first)) {
+                        canList.add(i);
+                    }
+                }
+                if (canList.size() != 0) {
+                    System.out.println("可以被整除的数: " + canList.toString());
+                }else{
+                    System.out.println("没有可以被整除的数");
                 }
             }
         }
 
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Work01 work01 = new Work01();
         work01.input();
     }
 
-
-    public boolean checkNum(String[] arr){
-        for (int i = 0; i< arr.length; i++){
-            if (!isNumberic(arr[i])) {
+    public boolean canDivInArr(int dividend, String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (dividend % Integer.parseInt(arr[i]) != 0) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean isNumberic(String s){
-        if (s==null || s.trim().length() ==0) {
+    public boolean checkNum(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (!isNumberic(arr[i])) {
+                return false;
+            }
+            if (arr[i].equals("0")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isNumberic(String s) {
+        if (s == null || s.trim().length() == 0) {
             return false;
         }
         String rule = "(-|\\+)?([0-9]+)";
         if (s.matches(rule)) {
             return true;
-        } else{
+        } else {
             return false;
         }
     }
